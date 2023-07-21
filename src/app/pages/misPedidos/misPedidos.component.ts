0413d12f22ef31e3c4d402bb6389a9c1caf32a7e
jaime <pepefloress.1999@gmail.com>
2023-07-21 13:41:17 -0600

    se modifico el orden en detalles y tambien ya se agarra las ventas de la bases de datos mejor

--- src/app/pages/misPedidos/misPedidos.component.ts
+++ src/app/pages/misPedidos/misPedidos.component.ts
@@ -47,34 +47,41 @@ export class misPedidos  implements OnInit{
     this.NumeroEmpleado = userauth.data.Numero_Empleado;
 
 
+
     this.numerodeUsuario = userauth.UsuarioId;
+    console.log(userauth);
 
 
     // es para ver solo los del el RhUsuarioId
     this.appService.GetVentasEmpleadoPorId(userauth.UsuarioId).subscribe((res)=>{
-      this.Ventas = res.filter((venta) => venta.RhUsuarioId === userauth.UsuarioId);
+      this.Ventas = res;
 
       //this.Ventas=res;
-
       console.log(this.Ventas);
-      console.log(userauth.UsuarioId)
-      //this.numeroDeVenta = Ventas.ventaEmpleadoId;
-
-      for (let venta of this.Ventas) {
-        let idEstatusVentaEmpleado = venta.idEstatusVentaEmpleado;
-        console.log('Valor de idEstatusVentaEmpleado:', venta.IdEstatusVentaEmpleado);
-
-        // Verificar el valor de idEstatusVentaEmpleado y mostrar el botón en caso de ser 1
-        if (idEstatusVentaEmpleado === 1) {
-          // this.mostrarBotonCancelar = true; c
-          console.log("es uno ");
-        } else {
-          // this.mostrarBotonCancelar = false;
-          console.log("no es uno");
-        }
-      }
 
     });
+
+
+
+    //   // de las venta debo agarrar el idStatus y colocarlo en status
+    //   console.log(userauth.UsuarioId)
+    //   //this.numeroDeVenta = Ventas.ventaEmpleadoId;
+
+    //   for (let venta of this.Ventas) {
+    //     let idEstatusVentaEmpleado = venta.idEstatusVentaEmpleado;
+    //     console.log('Valor de idEstatusVentaEmpleado:', venta.IdEstatusVentaEmpleado);
+
+    //     // Verificar el valor de idEstatusVentaEmpleado y mostrar el botón en caso de ser 1
+    //     if (idEstatusVentaEmpleado === 1) {
+    //       // this.mostrarBotonCancelar = true; c
+    //       console.log("es uno ");
+    //     } else {
+    //       // this.mostrarBotonCancelar = false;
+    //       console.log("no es uno");
+    //     }
+    //   }
+
+    // });
 
 
   //   this.appService.Data.cartList.forEach(product => {
@@ -90,7 +97,7 @@ export class misPedidos  implements OnInit{
       this.appService.CancelarVenta(id).subscribe((res) => {
 
         //location.reload();  // hace que la pagina se recargue automaticamente
-        this.actualizarDatosLocalmente(id);
+        //this.actualizarDatosLocalmente(id);
 
       })
     }
