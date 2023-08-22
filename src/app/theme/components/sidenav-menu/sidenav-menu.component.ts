5ab88fb16fe8817439d672b93221dab6ac151fe2
jaime <pepefloress.1999@gmail.com>
2023-08-22 12:48:43 -0600

    ahora en responsivo el usuario normal no puede ver lo de el admin en el menu

--- src/app/theme/components/sidenav-menu/sidenav-menu.component.ts
+++ src/app/theme/components/sidenav-menu/sidenav-menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SidenavMenuService } from './sidenav-menu.service';
 import { AppService } from 'src/app/app.service';
 import { Route } from '@angular/router';
+import { SidenavMenu } from './sidenav-menu.model';
 
 
 @Component({
@@ -14,27 +15,33 @@ export class SidenavMenuComponent implements OnInit {
   @Input('menuItems') menuItems;
   @Input('menuParentId') menuParentId;
   parentMenu:Array<any>;
+
   mostrarApartado: boolean= false;
+  router: any;
+  searchText: any;
+
 
   constructor(private sidenavMenuService:SidenavMenuService, public appService1:AppService ) { }
 
   ngOnInit() {
-    this.parentMenu = this.menuItems.filter(item => item.parentId == this.menuParentId);
-    // this.mostrarAdmin();
-    // todo es para mostrar a solo los admin
-    this.parentMenu = this.menuItems.filter(item => item.parentId === this.menuParentId);
+    // this.parentMenu = this.menuItems.filter(item => item.parentId == this.menuParentId); //!esto muestra las opciones
+    this.mostrarAdmin();
 
-  // Filtrar los elementos de menú para separar los que son "Administración" y los demás
-  const adminMenu = this.parentMenu.filter(item => item.esAdmin);
-  const otherMenus = this.parentMenu.filter(item => !item.esAdmin);
+  //   this.mostrarAdmin();
+  //   // todo es para mostrar a solo los admin
+  //   this.parentMenu = this.menuItems.filter(item => item.parentId === this.menuParentId);
 
-  // Si el usuario actual es administrador, agregamos los elementos de menú "Administración" al array de otros menús.
-  if (this.mostrarApartado) {
-    this.parentMenu = otherMenus.concat(adminMenu);
-  } else {
-    this.parentMenu = otherMenus;
-  }
-  // todo se acaba para mostrar a solo los admin
+  // // Filtrar los elementos de menú para separar los que son "Administración" y los demás
+  // const adminMenu = this.parentMenu.filter(item => item.esAdmin);
+  // const otherMenus = this.parentMenu.filter(item => !item.esAdmin);
+
+  // // Si el usuario actual es administrador, agregamos los elementos de menú "Administración" al array de otros menús.
+  // if (this.mostrarApartado) {
+  //   this.parentMenu = otherMenus.concat(adminMenu);
+  // } else {
+  //   this.parentMenu = otherMenus;
+  // }
+  // // todo se acaba para mostrar a solo los admin
 
   }
 
@@ -52,12 +59,37 @@ export class SidenavMenuComponent implements OnInit {
       if (res !== null && res.includes(userauth.data.INUsuarioId)) {
         this.mostrarApartado = true;
         console.log("Mostrar el apartado para el usuario actual");
+        if (this.mostrarApartado) {
+          this.parentMenu = this.menuItems.filter(item => item.parentId === this.menuParentId);
+        }
       } else {
         this.mostrarApartado = false;
         console.log("No mostrar el apartado para el usuario actual");
+
+        this.parentMenu = this.menuItems.filter(
+          item => item.title === 'PRODUCTOS' || item.title === 'MIS PEDIDOS' || item.title === 'Cerrar Sesion'
+        );
       }
     });
   }
+
+
+  // para buscar en la categoria
+
+  search() {
+    let queryParams: any = {};
+    queryParams.textSearch = this.searchText;
+
+    // Filtrar opciones de menú según el texto de búsqueda
+    this.parentMenu = this.menuItems.filter(item =>
+      item.title.includes(this.searchText) || item.title === 'Quimicos' || item.title === 'Equipos'
+    );
+
+    this.router.routeReuseStrategy.shouldReuseRoute = function () { return false; }
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/productos', this.searchText], { queryParams: queryParams });
+  }
+
 
 
 
--- src/app/theme/components/sidenav-menu/sidenav-menu.ts
+++ src/app/theme/components/sidenav-menu/sidenav-menu.ts
@@ -1,9 +1,14 @@
 import { SidenavMenu } from './sidenav-menu.model';
 import { SidenavMenuComponent } from './sidenav-menu.component';
 
+
+
 export const sidenavMenuItems = [
     // new SidenavMenu (1, 'Inicio', '/', null, null, false, 0),
-    new SidenavMenu (1, 'Inicio', '/productos', null, null, false, 0),
+
+    // new SidenavMenu (1, 'PRODUCTOS', '/productos', null, null, false, 0),
+
+    new SidenavMenu (1, 'PRODUCTOS', '/productos', null, null, false, 0),
 
 
     //   new SidenavMenu (2, 'Divisiones de Negocio', null, null, null, true, 0),
@@ -20,9 +25,13 @@ export const sidenavMenuItems = [
     // new SidenavMenu (33, 'Industrial', '/catalogosindustrial', null, null, false, 3),
 
     // new SidenavMenu (4, 'Categorías', null, null, null, true, 0),
-    // //new SidenavMenu (10, 'Químicos', null, null, null, false, 2),
-    // new SidenavMenu (41, 'Químicos', "/productos", null, null, false, 4),
-    // new SidenavMenu (22, 'Equipos', "/productos", null, null, false, 4),
+    // new SidenavMenu (41, 'Químicos', "/productos/", null, null, false, 4),
+    // new SidenavMenu (22, 'Equipos', "/productos ", null, null, false, 4),
+
+    // new SidenavMenu (10, 'Químicos', null, null, null, false, 4),
+
+
+
 
     // new SidenavMenu (50, '¿Quines somos?', '/quienessomos', null, null, false, 0),
     // new SidenavMenu (51, 'sucursales', '/sucursales', null, null, false, 0),
@@ -31,12 +40,12 @@ export const sidenavMenuItems = [
     // new SidenavMenu (54, 'Intranet', '/plataformas', null, null, false, 0),
 
     // new SidenavMenu (54, 'PRODUCTOS', '/productos', null, null, false, 0),
-    new SidenavMenu (54, 'PEDIDOS', '/misPedidos', null, null, false, 0),
+    new SidenavMenu (54, 'MIS PEDIDOS', '/misPedidos', null, null, false, 0),
     new SidenavMenu (54, 'CONSOLIDADOS', '/pedidosConsolidados', null, null, false, 0),
+    new SidenavMenu (1, 'Administracion', '/admin/products/product-list', null, null, false, 0),
+
     new SidenavMenu (1, 'Cerrar Sesion', '/', null, null, false, 0),
 
-
-    new SidenavMenu (1, 'Administracion', '/admin', null, null, false, 0),
 
 
 
@@ -51,7 +60,7 @@ export const sidenavMenuItems = [
     // new SidenavMenu (64, 'NAV.CART', '/cart', null, null, false, 61),
     // new SidenavMenu (65, 'NAV.CHECKOUT', '/Cotizacion', null, null, false, 61),
     // new SidenavMenu (65, 'NAV.CHECKOUT', '/checkout', null, null, false, 61),
-    new SidenavMenu (70, 'LOGIN', '/sign-in', null, null, false, 60),
+    // new SidenavMenu (70, 'LOGIN', '/sign-in', null, null, false, 60),
     // new SidenavMenu (71, 'NAV.404_PAGE', '/404', null, null, false, 60),
     // new SidenavMenu (72, 'NAV.LANDING', '/landing', null, null, false, 60),
     // new SidenavMenu (80, 'NAV.CONTACT', '/contact', null, null, false, 0),
